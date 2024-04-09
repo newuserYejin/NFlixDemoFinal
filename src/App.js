76c15bfb526e455421400c9fdffb4d09695acb5b
jin <jin200126@gmail.com>
2024-04-10 01:38:43 +0900

    route update

--- src/App.js
+++ src/App.js
@@ -13,18 +13,19 @@ import NotFound from './pages/NotFound/NotFound';
 // 영화 전체보여주는 페이지(검색가능)     /movies
 // 영화 디테일 페이지         /movies/:id
 
+/* 부모 밑에 그냥 붙이겠단 표시로 index(path = '/' 과 같은 효과) */
+/* <Route path='/movies' element={<MoviePage />} />
+<Route path='/movies/:id' element={<MovieDetailPage />} /> */
 function App() {
   return (
     <div className='body'>
       <Routes>
         <Route path='/' element={<AppLayout />}>
-          <Route index element={<Homepage />} />    {/* 부모 밑에 그냥 붙이겠단 표시로 index(path = '/' 과 같은 효과) */}
+          <Route index element={<Homepage />} />
           <Route path='movies'>
             <Route index element={<MoviePage />} />
             <Route path=':id' element={<MovieDetailPage />} />
           </Route>
-          {/* <Route path='/movies' element={<MoviePage />} />
-          <Route path='/movies/:id' element={<MovieDetailPage />} /> */}
         </Route>
 
         <Route path='*' element={<NotFound />} />
--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+// 이동 경로 2가지
+// 네비바에서 검색 ( popular movie 출력 )
+// 키워드 입력 ( 키워드와 관련된 영화 출력)
 const MoviePage = () => {
   return <div>MoviePage</div>;
 };
